6425dcb7dd72c65b58f0a858ce03d2e6328fc51b
mgutz <mario@mgutz.com>
2010-02-24 15:14:50 -0800

    track changes uses CRC32

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -53,6 +53,8 @@ end
 
 
 def add_update_item_attributes
+  changes = MGutz::FileChanges.new
+
   items.each do |item|
     # do not include assets or xml files in sitemap
     if item[:content_filename]
@@ -60,14 +62,19 @@ def add_update_item_attributes
       item[:is_hidden] = true if item[:content_filename] =~ /assets\// || ext == '.xml'
     end
 
-    next if item[:kind] != "article"
+    if item[:kind] == "article"
+      # filename might contain the created_at date
+      item[:created_at] ||= derive_created_at(item)
+      # sometimes nanoc3 stores created_at as Date instead of String causing a bunch of issues
+      item[:created_at] = item[:created_at].to_s if item[:created_at].is_a?(Date)
 
-    # filename might contain the created_at date
-    item[:created_at] ||= derive_created_at(item)
-    # sometimes nanoc3 stores created_at as Date instead of String causing a bunch of issues
-    item[:created_at] = item[:created_at].to_s if item[:created_at].is_a?(Date)
+      # sets updated_at based on content change date not file time
+      change = changes.status(item[:content_filename], item[:created_at], item.raw_content)
+      item[:updated_at] = change[:updated_at].to_s
+    end
   end
 end
+
 
 # Copy static assets outside of content instead of having nanoc3 process them.
 def copy_static
